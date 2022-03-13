d1b518a36edca1bb1ad3bf0d0405776388cb38e9
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-13 10:50:35 -0700

    print combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lovercase
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		putchar(ch);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

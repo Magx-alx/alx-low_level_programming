f1666b6465ff01f05edc932ddf657dbc0a1f86d6
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-13 03:45:15 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,6 +17,21 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
   	ld= n % 10;
-	printf("%d is the last digit for number %d", ld, n);
+	if (ld > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5",n , ld);
+	}
+	else
+	{
+		if (ld == 0)
+		{
+			printf("Last digit of %d is %d and is 0",n , ld);
+		}
+		else
+		{
+			printf("Last digit of %d is %d and is less than 6 and not 0",n , ld);
+		}
+	}	
+
 	return (0);
 }

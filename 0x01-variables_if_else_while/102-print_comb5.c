eedc5f1bdcc26956058927ce4e7718da5c459039
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-13 13:50:02 -0700

    102-print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lovercase
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j =0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(' ');
+			for (k =i; k < 10; k++)
+			{
+				for (l = k + 1; l < 10; l++)
+				{
+					putchar(k + '0');
+					putchar(l + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

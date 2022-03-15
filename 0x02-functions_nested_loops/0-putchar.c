cf50a9af47a5d626897fd29af8156f2135579c09
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-15 11:04:02 -0700

    print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: the function that prints _putchar
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#!/bin/bash
+0x02-fuctions and nested loops readme file

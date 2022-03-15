0d830965fb0a6d417241a864a882a274e30ffb94
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-15 15:13:56 -0700

    alphabet printer

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -12,3 +12,13 @@ int main ()
 	print_alphabet();
 	return (0);
 }
+void print_alphabet (void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,4 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
-
-void print_alphabet(void)
-{
-	char c = 'a';
-	while (c <= 'z')
-	{
-		_putchar(c);
-		c++;
-		_putchar('\n');
-	}
-}
+#endif

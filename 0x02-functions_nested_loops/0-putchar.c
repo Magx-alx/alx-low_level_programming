79429610256ede2100042ef9aad301c296ac3e30
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-15 12:17:40 -0700

    print alphabets

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -18,7 +18,3 @@ int main(void)
 	_putchar('\n');
 	return (0);
 }
-void _putchar(char c)
-{
-	putchar(c);
-}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,2 +1,15 @@
-void _putchar(char c);
-void print_alphabet(void);
+int _putchar(char c)
+{
+    putchar(c);
+}
+
+void print_alphabet(void)
+{
+	char c = 'a';
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar('\n');
+}

8d1f76f680710eda61b24ab694e9ee0031cf2215
Magx-alx <alexgabrielmalisa@gmail.com>
2022-03-15 14:03:59 -0700

    print alphabets

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,5 +9,13 @@
 
 int main ()
 {
-	printf("magx");
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+	return (0);
 }
--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,5 +9,6 @@
 
 int main ()
 {
-	printf("magx");
+	print_alphabet();
+	return (0);
 }

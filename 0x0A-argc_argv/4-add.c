46ba5fd97d08d00dde872e937c061d1dcd6ac68d
scansel <scanseltanzania@gmail.com>
2022-04-01 18:08:02 +0300

    add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,58 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
 
 /**
- * _isnumber - checks if string is a number
- * @s: string
- *
- * Return: On success 1.
- * If not a number, 0 is returned.
+ *main-entry point
+ *@argc:1st parameter
+ *@argv:2nd parameter
+ *Return:0 if successful
+ *Solution credit to Hileamlak
  */
 
-int _isnumber(char *s)
+int main(int argc, char *argv[])
 {
-	int i, check, d;
-
-	i = 0, d = 0, check = 1;
-	if (*s == '-')
-		i++;
-	for (; *(s + i) != 0; i++)
-	{
-		d = isdigit(*(s + i));
-		if (d == 0)
-		{
-			check = 0;
-			break;
-		}
-	}
-	return (check);
-}
-/**
- * main - Entry point
- *
- * @argc: Counts the number of parameters that go into main
- * @argv: Pointer of array of pointers containing strings entering main
- * Return: Always 0 (Success)
- */
-int main(int argc, char **argv)
-{
-	int i, n, ex;
-
-	ex = 0, n = 0;
-	if (argc > 1)
-	{
-		for (i = 1; i < argc; i++)
-		{
-			if (_isnumber(argv[i]))
-				n += atoi(argv[i]);
-			else
-				ex = 1;
-		}
-	}
-	if (ex == 0)
-		printf("%i\n", n);
-	else
-		printf("%s\n", "Error");
-	return (ex);
+	int i, j, res = 0;
+    
+    if (argc > 1)
+        for (i = 1; i < argc; i++)
+        {
+            for (j = 0; argv[i][j]; j++)
+                if (argv[i][j] < '0' || argv[i][j] > '9')
+                    return (printf("Error\n"), 1);
+            res += atoi(argv[i]);
+        }
+    printf("%i\n", res);
+    return (0);
 }
